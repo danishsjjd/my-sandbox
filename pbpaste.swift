e37a940076925a4c502edd6020d944e1878081e0
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2024-05-17 15:32:35 +0500

    feat: swift get clipboard html

---
+++ pbpaste.swift
@@ -0,0 +1,10 @@
+import Cocoa
+let type = NSPasteboard.PasteboardType.html
+if let string = NSPasteboard.general.string(forType:type) {
+  print(string)
+}
+else {
+  print("Could not find string data of type '\(type)' on the system pasteboard")
+  exit(1)
+}
+
