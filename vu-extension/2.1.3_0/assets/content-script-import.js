5759485706b6a6d85169a85e6ca98c9baa20b6d4
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2025-04-29 22:45:21 +0500

    fix: remove vu crack extension

--- vu-extension/2.1.3_0/assets/content-script-import.js
+++
@@ -1,1 +0,0 @@
-;(()=>{const runtime = chrome?.runtime || browser?.runtime; runtime ? import(runtime.getURL("assets/content-script.js")) : (document?.currentScript?.src && import(new URL("/assets/content-script.js", document.currentScript.src).href))})()
